ead81e217379f32fa80ec2bacd06fd4fe314b71a
Wiliam Sariol <wvendetta9041@gmail.com>
2020-02-19 18:15:13 -0500

    add class serializer to manage specific put requests in account resource

--- challenge/accounts/serializers.py
+++ challenge/accounts/serializers.py
@@ -7,3 +7,8 @@ class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Account
         fields = '__all__'
+
+class AccountUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Account
+        fields = ('phone', 'shipping_address1', 'shipping_address2', 'shipping_city', 'shipping_state', 'shipping_zip', 'shipping_country')
